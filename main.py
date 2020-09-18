f94885e4df872c157f20e5132ca839923b4d582d
Eryk Knesz <eryk.knesz@gmail.com>
2020-09-18 20:25:55 +0200

    Add last loop and print with total of products

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ lista_zakupów = {
      "warzywniak": ["Chleb", "Pączek", "Bułki"],
     "Piekarnia": ["Marchew", "Seler", "Rukola"]
 }
-
+count = 0
 for sklep, produkt in lista_zakupów.items():
     print(f"Idę do {sklep.upper()} i kupuję tam następujące rzeczy {str(produkt).upper()}")
+    for item in produkt:
+        count = count + 1
+print(f"W sumie kupuję {count} produktów")
