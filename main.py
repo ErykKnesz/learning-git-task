20d6e2e47a60cd0222adcce613363c5fcc050d77
Eryk Knesz <eryk.knesz@gmail.com>
2020-09-18 20:21:43 +0200

    Add first for loop and print

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-shopping_list = {
+lista_zakupów = {
      "warzywniak": ["Chleb", "Pączek", "Bułki"],
     "Piekarnia": ["Marchew", "Seler", "Rukola"]
 }
 
+for sklep, produkt in lista_zakupów.items():
+    print(f"Idę do {sklep} i kupuję tam następujące rzeczy {str(produkt)}")
